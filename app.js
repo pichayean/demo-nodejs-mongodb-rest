fd627beba91745a4fd80dadac0c8b10da1e5e728
pichayean <pichayeanyensiri@gmail.com>
2020-08-30 16:35:43 +0700

    -

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 mongodb.connectDB();
 
